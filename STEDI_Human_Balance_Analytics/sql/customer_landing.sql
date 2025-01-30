a97175178a98b15972af0ed3becb645d1b1bb6e4
Lucas Furlan <155035762+furlanflucas@users.noreply.github.com>
2025-01-30 10:01:36 -0600

    Update customer_landing.sql

--- STEDI_Human_Balance_Analytics/sql/customer_landing.sql
+++ STEDI_Human_Balance_Analytics/sql/customer_landing.sql
@@ -1,17 +1,32 @@
-CREATE EXTERNAL TABLE `stedi.customer_landing` (
-    `customer_id` STRING COMMENT 'from deserializer',
-    `email` STRING COMMENT 'from deserializer',
-    `registration_date` TIMESTAMP COMMENT 'from deserializer',
-    `share_with_research` BOOLEAN COMMENT 'from deserializer'
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-WITH SERDEPROPERTIES (
-    'paths'='customer_id,email,registration_date,share_with_research'
-)
-STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat'
-OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://customer--bucket/customer_landing/'
+CREATE EXTERNAL TABLE `stedi.customer_landing_folder`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='birthDay,customerName,email,lastUpdateDate,phone,registrationDate,serialNumber,shareWithFriendsAsOfDate,shareWithPublicAsOfDate,shareWithResearchAsOfDate') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://customer--bucket/customer_landing_folder/'
 TBLPROPERTIES (
-    'classification'='json',
-    'typeOfData'='file'
-);
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='customer_table_creation', 
+  'averageRecordSize'='307', 
+  'classification'='json', 
+  'compressionType'='none', 
+  'objectCount'='1', 
+  'recordCount'='956', 
+  'sizeKey'='293777', 
+  'typeOfData'='file')
